1f0a196b14e6fea081a39fdf2eb4d0c5941b52c0
Brett Gamble <brettkgamble@gmail.com>
2026-01-07 08:25:59 -0700

    0003 - Day 5 Simple Person Figure

---
+++ Tutorials/Day_4/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+  strokeWeight(1);
+  ellipse(300, 120, 50);
+  ellipse(290, 115, 10);
+  ellipse(310, 115, 10);
+  line(300, 115, 300, 125);
+  line(293, 132, 307, 132);
+  rect(265, 145, 70, 80);
+  strokeWeight(10);
+  line(280, 230, 280, 290);
+  line(320, 230, 320, 290); 
+  line(261, 160, 220, 180);
+  line(339, 160, 380, 180);
+}
